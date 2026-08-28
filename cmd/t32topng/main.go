2377207db6264008063120093af46d8e178176e6
anasrar <mynameanasrar@gmail.com>
2026-08-28 21:02:20 +0900

    refactor(t32topng): change from positional to flags

--- cmd/t32topng/main.go
+++ cmd/t32topng/main.go
@@ -14,91 +14,95 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var input string
+
+var cmd = &cobra.Command{
+	Use:   "t32topng",
+	Short: "Convert T32 to PNG",
+	Run: func(cmd *cobra.Command, args []string) {
+		pathDirectoryFiles, pathManifest, err := unpackPath(input)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if err := os.MkdirAll(pathDirectoryFiles, 0755); err != nil {
+			log.Fatal(err)
+		}
+
+		file, err := os.Open(input)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+
+		container := t32.New()
+		if err := t32.FromStream(container, file); err != nil {
+			log.Fatal(err)
+		}
+
+		m := metadata.MetadataT32{Textures: make([]metadata.TextureT32, 0)}
+
+		for textureIndex, texture := range container.Textures {
+			if !texture.IsIndexed() {
+				log.Printf(
+					"skip texture %d: pixel format %#x is not indexed",
+					textureIndex,
+					uint8(texture.GsTex0.PSM),
+				)
+				continue
+			}
+
+			tx := metadata.TextureT32{
+				Index:    uint64(texture.Index),
+				Palettes: make([]metadata.PaletteT32, 0),
+			}
+
+			for paletteIndex := 0; paletteIndex < int(texture.PaletteTotal); paletteIndex++ {
+				decoded, err := container.DecodeTexture(textureIndex, paletteIndex)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				filename := fmt.Sprintf("texture_%03d_palette_%03d.png", textureIndex, paletteIndex)
+				outputPath := filepath.Join(pathDirectoryFiles, filename)
+				if err := writePNG(outputPath, decoded.Image()); err != nil {
+					log.Fatal(err)
+				}
+
+				log.Printf("create: %s", filename)
+
+				tx.Palettes = append(tx.Palettes, metadata.PaletteT32{
+					Index: uint64(paletteIndex),
+					Path:  outputPath,
+				})
+			}
+
+			m.Textures = append(m.Textures, tx)
+		}
+
+		fileManifest, err := os.Create(pathManifest)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+
+		j, err := json.MarshalIndent(m, "", "  ")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if _, err := fileManifest.Write(j); err != nil {
+			log.Fatal(err)
+		}
+	},
+}
+
+func init() {
+	cmd.Flags().StringVarP(&input, "input", "i", "", "Input file")
+	cmd.MarkFlagRequired("input")
+}
+
 func main() {
-	cmd := &cobra.Command{
-		Use:   "t32topng [T32]",
-		Short: "Convert T32 to PNG",
-		Args:  cobra.ExactArgs(1),
-		Run: func(cmd *cobra.Command, args []string) {
-
-			pathFromArgs := args[0]
-			pathDirectoryFiles, pathManifest, err := unpackPath(pathFromArgs)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			if err := os.MkdirAll(pathDirectoryFiles, 0755); err != nil {
-				log.Fatal(err)
-			}
-
-			file, err := os.Open(pathFromArgs)
-			if err != nil {
-				log.Fatal(err)
-			}
-			defer file.Close()
-
-			container := t32.New()
-			if err := t32.FromStream(container, file); err != nil {
-				log.Fatal(err)
-			}
-
-			m := metadata.MetadataT32{Textures: make([]metadata.TextureT32, 0)}
-
-			for textureIndex, texture := range container.Textures {
-				if !texture.IsIndexed() {
-					log.Printf(
-						"skip texture %d: pixel format %#x is not indexed",
-						textureIndex,
-						uint8(texture.GsTex0.PSM),
-					)
-					continue
-				}
-
-				tx := metadata.TextureT32{
-					Index:    uint64(texture.Index),
-					Palettes: make([]metadata.PaletteT32, 0),
-				}
-
-				for paletteIndex := 0; paletteIndex < int(texture.PaletteTotal); paletteIndex++ {
-					decoded, err := container.DecodeTexture(textureIndex, paletteIndex)
-					if err != nil {
-						log.Fatal(err)
-					}
-
-					filename := fmt.Sprintf("texture_%03d_palette_%03d.png", textureIndex, paletteIndex)
-					outputPath := filepath.Join(pathDirectoryFiles, filename)
-					if err := writePNG(outputPath, decoded.Image()); err != nil {
-						log.Fatal(err)
-					}
-
-					log.Printf("create: %s", filename)
-
-					tx.Palettes = append(tx.Palettes, metadata.PaletteT32{
-						Index: uint64(paletteIndex),
-						Path:  outputPath,
-					})
-				}
-
-				m.Textures = append(m.Textures, tx)
-			}
-
-			fileManifest, err := os.Create(pathManifest)
-			if err != nil {
-				log.Fatal(err)
-			}
-			defer file.Close()
-
-			j, err := json.MarshalIndent(m, "", "  ")
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			if _, err := fileManifest.Write(j); err != nil {
-				log.Fatal(err)
-			}
-		},
-	}
-
 	cmd.Execute()
 }
 

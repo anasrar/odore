079d6a9e729a1d431683dbd0e6efba4f05f64a48
anasrar <mynameanasrar@gmail.com>
2026-09-05 05:23:55 +0200

    feat(utils): error signature is not match

--- pkg/utils/errors.go
+++ pkg/utils/errors.go
@@ -1,9 +1,16 @@
 package utils
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 var (
 	ErrNotIndexed     = errors.New("texture is not PSMT8 or PSMT4 indexed data")
 	ErrContainerIsNil = errors.New("Container is nil")
 	ErrStreamIsNil    = errors.New("stream is nil")
 )
+
+func ErrSignatureIsNotMatch(expect, actual uint32) error {
+	return fmt.Errorf("signature is not match: expect %#x, actual: %#x", expect, actual)
+}
